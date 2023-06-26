9da602227ea6eff146a72c950eb2f6e9e7c6314a
Ganapati <ganapatasakatwa@gmail.com>
2023-06-26 06:33:46 +0530

    [GK] UC2 Create Table

--- ADOPayRool/ADOProb.cs
+++ ADOPayRool/ADOProb.cs
@@ -30,6 +30,7 @@ namespace ADOPayRool
         {
             try
             {
+
                 SqlConnection con = new SqlConnection("data source=(localdb)\\MSSQLLocalDB ; initial catalog=payroll_service ; integrated security= true");
                 con.Open();
                 string query = "create table employee_payroll (id int identity(1,1) primary key, name varchar(200),salary int, startdate date)";
